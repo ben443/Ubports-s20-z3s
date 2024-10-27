dbf1ca40405d590a174eb8f303951d7249470c46
snapdragon teaks <97859152+Elchanz3@users.noreply.github.com>
2024-10-27 01:36:07 -0300

    flashligth_permission.sh : add support for c1s device permission handling
    Extended support for changing ownership of rear_flash permission to "phablet" user 
    when the device is either "r8s" or "c1s". Previously, this permission change was 
    applied only to "r8s". The addition of the "elif" statement allows the script to 
    handle both devices separately, maintaining modularity for future adjustments 
    specific to each device model.
    
    This change ensures that c1s users have the correct permissions setup, improving 
    compatibility with device-specific operations.

--- overlay/system/usr/libexec/lxc-android-config/device-hacks.d/flashligth_permission.sh
+++ overlay/system/usr/libexec/lxc-android-config/device-hacks.d/flashligth_permission.sh
@@ -4,8 +4,13 @@ DEVICE=$(getprop ro.product.device)
 
 if [ "$DEVICE" == "r8s" ]; then
     sleep 0.5
-    
+    if [ -e /sys/class/camera/rear_flash ]; then
+        sudo chown phablet /sys/class/camera/rear_flash
+    fi
+elif [ "$DEVICE" == "c1s" ]; then
+    sleep 0.5
     if [ -e /sys/class/camera/rear_flash ]; then
         sudo chown phablet /sys/class/camera/rear_flash
     fi
 fi
+
