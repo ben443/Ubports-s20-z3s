d9033372cd5a730f2c31b68928d47388cec054ed
snapdragon teaks <inutilidades639@gmail.com>
2024-10-28 01:37:15 -0300

    overlay: device-hacks: flashlight: Refactor script to consolidate device checks and improve readability

--- overlay/system/usr/libexec/lxc-android-config/device-hacks.d/flashlight_permission.sh
+++ overlay/system/usr/libexec/lxc-android-config/device-hacks.d/flashlight_permission.sh
@@ -2,12 +2,7 @@
 
 DEVICE=$(getprop ro.product.vendor.device)
 
-if [ "$DEVICE" == "r8s" ]; then
-    sleep 0.5
-    if [ -e /sys/class/camera/rear_flash ]; then
-        sudo chown phablet /sys/class/camera/rear_flash
-    fi
-elif [ "$DEVICE" == "c1s" ]; then
+if [[ "$DEVICE" == "r8s" || "$DEVICE" == "c1s" ]]; then
     sleep 0.5
     if [ -e /sys/class/camera/rear_flash ]; then
         sudo chown phablet /sys/class/camera/rear_flash
