5c5b38e4f6dfaf022ef546b7ff08063462af8a45
Denzeel Oliva <wachiturroxd150@gmail.com>
2024-11-25 23:13:36 -0500

    overlay: sbin: update shortcut shell
    To ensure that the supplied arguments are passed correctly to the Android container.
    
    Signed-off-by: Denzeel Oliva <wachiturroxd150@gmail.com>

--- overlay/system/usr/sbin/android_shell.sh
+++ overlay/system/usr/sbin/android_shell.sh
@@ -1,1 +1,9 @@
-sudo bash -c 'LD_PRELOAD="" LD_LIBRARY_PATH="/system/lib64:/vendor/lib64" lxc-attach -n android '"$@"
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    exit 1
+fi
+
+CMD="$@"
+
+sudo bash -c "LD_PRELOAD='' LD_LIBRARY_PATH='/system/lib64:/vendor/lib64' lxc-attach -n android -- $CMD"
